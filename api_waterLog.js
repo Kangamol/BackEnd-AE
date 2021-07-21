471d436e694b66a93d0b7b571e8345872539fe6a
Kangamol <kangamol8520@gmail.com>
2021-07-21 13:31:57 +0700

    initail commit

--- api_waterLog.js
+++ api_waterLog.js
@@ -12,21 +12,22 @@ let checkSendLine = true
 
 setInterval( async() => {
     let timeCheck = new Date();
-    console.log(timeCheck)
+    console.log(timeCheck - 1)
     console.log(timeCheck.getHours())
     console.log(timeCheck.getMinutes())
-    if(timeCheck.getHours() === 18 && timeCheck.getMinutes() >= 6 && timeCheck.getMinutes() <= 6 && checkSendLine) {
+    if(timeCheck.getHours() === 8 && timeCheck.getMinutes() >= 36 && timeCheck.getMinutes() <=36 && checkSendLine) {
          try {
             const pool = await poolPromise;
             const result3 = await pool.request().query(`
-            SELECT	[WT-01]AS WT1, [WT-02]AS WT2, [WT-03]AS WT3, [WT-04]AS WT4, [WT-05]AS WT5, [WT-06]AS WT6,
+            SELECT	CONVERT(VARCHAR,(GETDATE() - 1),103)AS DateShow,
+                    [WT-01]AS WT1, [WT-02]AS WT2, [WT-03]AS WT3, [WT-04]AS WT4, [WT-05]AS WT5, [WT-06]AS WT6,
 					[WT-07]AS WT7, [WT-08]AS WT8, [WT-09]AS WT9, [WT-10]AS WT10, [WT-11]AS WT11, [WT-12]AS WT12
 					FROM(SELECT	waterLimit.deviceCode, ISNULL(DataCount * exrate,0)AS Total
 							FROM	waterLimit LEFT JOIN
 							(
 							SELECT		deviceCode, CAST(transDate AS date)AS transDate, COUNT(*)AS DataCount
 							FROM		waterLog
-							WHERE		CAST(transDate AS date) = CAST(GETDATE()AS date)
+							WHERE		CAST(transDate AS date) = CAST((GETDATE() - 1)AS date)
 							GROUP BY	deviceCode, CAST(transDate AS date)
 							) AS CountLog   ON CountLog.deviceCode = waterLimit.deviceCode)
                     AS PIVOTTABLE PIVOT (SUM(Total) FOR deviceCode IN ([WT-01],[WT-02],[WT-03],[WT-04],[WT-05],[WT-06],[WT-07],[WT-08],[WT-09],[WT-10],[WT-11],[WT-12]))AS RR
@@ -35,7 +36,7 @@ setInterval( async() => {
                 checkSendLine = false
                 let dataSendLine = result3.recordset[0]
                 await lineNotify.notify({
-                            message: `วันที่ ${timeCheck.toLocaleString()}
+                            message: `วันที่ ${dataSendLine.DateShow}
 WT-01 ใช้น้ำไป ${ dataSendLine.WT1 } ลิตร
 WT-02 ใช้น้ำไป ${ dataSendLine.WT2 } ลิตร
 WT-03 ใช้น้ำไป ${ dataSendLine.WT3 } ลิตร
@@ -73,7 +74,7 @@ WT-12 ใช้น้ำไป ${ dataSendLine.WT12 } ลิตร`,
         WHERE	(DataCount * exrate) >= rateLimit AND CAST(ISNULL(LastUpdateLine, '20210101') AS date) != CAST(GETDATE() AS date)      
         `)
         // let { deviceCode, transDate, DataCount, exrate, rateLimit, LastUpdateLine } = result.recordset[0]
-    //    console.log(' ทำงาน')
+        // console.log(' ทำงาน')
         if(result.recordset.length > 0) {
             // console.log(result.recordset)
             let data = result.recordset
@@ -83,14 +84,12 @@ WT-12 ใช้น้ำไป ${ dataSendLine.WT12 } ลิตร`,
                     const result2 = await pool2.request().query(`
                         UPDATE waterLimit SET LastUpdateLine = GETDATE()
                         WHERE deviceCode = '${ loop.deviceCode }'
-                    `)
-                        // console.log(result2)
-                        // console.log('ส่ง Line')
-                        await lineNotify.notify({
-                            message: `${ loop.deviceCode } = ใช้น้ำเกิน`,
-                        });
-
-
+                    `) // console.log('ส่ง Line')
+                        if(result2) {
+                            await lineNotify.notify({
+                                message: `${ loop.deviceCode } = ใช้น้ำเกิน`,
+                            });
+                        }
                 } catch { }
             });
         }
